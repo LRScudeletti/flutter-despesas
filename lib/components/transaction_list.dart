edf99ce222ba8d344a2d4d45fda9a52a52250358
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2021-11-20 16:53:07 -0300

    Ajustado Lista e Chart para obter tamanho dinâmico

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,62 +10,59 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 375,
-      child: transaction.isEmpty
-          ? Column(
-              children: <Widget>[
-                SizedBox(height: 10),
-                Text(
-                  'Nenhuma transação cadastrada!',
-                  style: Theme.of(context).textTheme.headline6,
+    return transaction.isEmpty
+        ? Column(
+            children: <Widget>[
+              SizedBox(height: 10),
+              Text(
+                'Nenhuma transação cadastrada!',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(height: 20),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemCount: transaction.length,
-              itemBuilder: (ctx, index) {
-                final tr = transaction[index];
-                return Card(
-                  elevation: 2,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 6,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text('R\$${tr.value}'),
-                        ),
+              )
+            ],
+          )
+        : ListView.builder(
+            itemCount: transaction.length,
+            itemBuilder: (ctx, index) {
+              final tr = transaction[index];
+              return Card(
+                elevation: 2,
+                margin: EdgeInsets.symmetric(
+                  vertical: 6,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text('R\$${tr.value}'),
                       ),
                     ),
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat('d MMM y').format(tr.date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => onRemove(tr.id),
-                    ),
                   ),
-                );
-              },
-            ),
-    );
+                  title: Text(
+                    tr.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat('d MMM y').format(tr.date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => onRemove(tr.id),
+                  ),
+                ),
+              );
+            },
+          );
   }
 }
