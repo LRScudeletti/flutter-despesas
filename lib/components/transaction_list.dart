ffc34c83097472eb5e28eef7ebfe1ad20110bff2
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2021-11-20 17:48:12 -0300

    Ajustado app para funcionar no modo landscape

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,22 +11,28 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transaction.isEmpty
-        ? Column(
-            children: <Widget>[
-              SizedBox(height: 10),
-              Text(
-                'Nenhuma transação cadastrada!',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              SizedBox(height: 20),
-              Container(
-                height: 200,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
-                ),
-              )
-            ],
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Nenhuma transação cadastrada!',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                ],
+              );
+            },
           )
         : ListView.builder(
             itemCount: transaction.length,
--- lib/main.dart
+++ lib/main.dart
@@ -88,6 +88,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: Text(
         'Despesas Pessoais',
@@ -110,28 +113,29 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Switch(
-                    value: _showChart,
-                    onChanged: (value) {
-                      setState(() {
-                        _showChart = value;
-                      });
-                    }),
-                Text("Exibir Gráfico"),
-              ],
-            ),
-            if (_showChart)
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Switch(
+                      value: _showChart,
+                      onChanged: (value) {
+                        setState(() {
+                          _showChart = value;
+                        });
+                      }),
+                  Text("Exibir Gráfico"),
+                ],
+              ),
+            if (_showChart || !isLandscape)
               Container(
                 width: double.infinity,
                 child: Container(
-                  height: availableHeight * 0.3,
+                  height: availableHeight * (isLandscape ? 0.7 : 0.3),
                   child: Chart(_recenteTransactions),
                 ),
-              )
-            else
+              ),
+            if (!_showChart || !isLandscape)
               Container(
                 height: availableHeight * 0.7,
                 child: TransactionList(_transactions, _removeTransaction),
